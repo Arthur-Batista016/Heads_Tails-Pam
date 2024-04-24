e19fb19777e5f69845957d408f415ce47d5fcfff
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2024-04-24 18:50:51 -0300

    feat: atualizei a interface

--- HeadsTails/HeadsTails/MainPage.xaml.cs
+++ HeadsTails/HeadsTails/MainPage.xaml.cs
@@ -38,10 +38,10 @@ namespace HeadsTails
             {
                 moeda.Source = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0c0483ff-7316-4928-957d-3d0b1b7e0ec7/d84p3mj-44fe4bd2-a539-499e-bfe7-f830d9dcd193.jpg/v1/fill/w_1600,h_1621,q_75,strp/cara___moeda_de_1__real_vetorizada_by_matheus8b_d84p3mj-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTYyMSIsInBhdGgiOiJcL2ZcLzBjMDQ4M2ZmLTczMTYtNDkyOC05NTdkLTNkMGIxYjdlMGVjN1wvZDg0cDNtai00NGZlNGJkMi1hNTM5LTQ5OWUtYmZlNy1mODMwZDlkY2QxOTMuanBnIiwid2lkdGgiOiI8PTE2MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.KhaoPG_8-7w5c52tXnSq2eE4q75hpbqlmrE2q07CkI4";
 
-                if (headButtonClicked){
+                if (headButtonClicked==true){
                     DisplayAlert("Cara", "Você Acertou!!", "Ok");
                 }
-                else
+                else if(headButtonClicked == false)
                 {
                     DisplayAlert("Cara", "Você Errou!!, tente novamente", "Ok");
                 }
@@ -50,10 +50,10 @@ namespace HeadsTails
             {
                 moeda.Source = "https://t4.ftcdn.net/jpg/05/64/35/03/360_F_564350370_LYxzggTtdpEvOMMUAxVFSTlTeQWfbh1L.jpg";
                
-                if (tailsButtonClicked){
+                if (tailsButtonClicked==true){
                     DisplayAlert("Coroa", "Você Acertou!!", "Ok");
                 }
-                else
+                else if(tailsButtonClicked == false)
                 {
                     DisplayAlert("Coroa", "Você Errou!!, tente novamente", "Ok");
                 }
