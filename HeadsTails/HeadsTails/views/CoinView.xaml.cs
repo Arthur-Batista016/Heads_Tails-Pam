cb6030e8b7992d27505b7d09bc4b65309f496fdd
Etec Prof. Horácio Augusto da Silveira <marcospborborema@hotmail.com>
2024-06-06 12:39:22 -0300

    feat:adicionei as views

--- HeadsTails/HeadsTails/views/CoinView.xaml.cs
+++ HeadsTails/HeadsTails/views/CoinView.xaml.cs
@@ -1,3 +1,5 @@
+using HeadsTails.viewModels;
+
 namespace HeadsTails.views;
 
 public partial class CoinView : ContentPage
@@ -5,5 +7,6 @@ public partial class CoinView : ContentPage
 	public CoinView()
 	{
 		InitializeComponent();
+		BindingContext = new CoinViewModel();
 	}
 }
